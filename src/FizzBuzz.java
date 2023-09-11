0ff87a19b61177754051c12893a9065765b4a3fd
Li Zhang <lijianzhang2@gmail.com>
2023-09-11 13:58:11 -0400

    changed readme.md

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,14 +5,15 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        int i = 1;
+        while (i < 100){
 
             doFizzBuzz(i);
-
+            i++;
         }
     }
 
-    private static void doFizzBuzz(int i) {
+    public static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
